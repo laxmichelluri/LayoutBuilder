c2a9a815b1dffbd63a0908712c65c7e3b6f60f09
laxmichelluri <laxmichelluri744@gmail.com>
2023-08-13 10:51:04 +0000

    LayoutBuilder

---
+++ src/components/Body/index.js
@@ -0,0 +1,50 @@
+// Write your code here
+
+import ConfigurationContext from '../../context/ConfigurationContext'
+
+import './index.css'
+
+const Body = () => (
+  <ConfigurationContext.Consumer>
+    {value => {
+      const {showLeftNavbar, showContent, showRightNavbar} = value
+
+      return (
+        <div className="body-container">
+          {showLeftNavbar ? (
+            <div className="left-navbar">
+              <h1 className="body-heading">Left Navbar Menu</h1>
+              <ul className="list-items">
+                <li className="item">Item 1</li>
+                <li className="item">Item 2</li>
+                <li className="item">Item 3</li>
+                <li className="item">Item 4</li>
+              </ul>
+            </div>
+          ) : null}
+          {showContent ? (
+            <div className="content">
+              <h1 className="body-heading">Content</h1>
+              <p className="content-description">
+                Lorem ipsum dolor sit amet, consector adipiscinq elit, sed do
+                eiused tempor incidinut ut labore et dalore magna aliqua. Ut
+                enim ad minim veniam.
+              </p>
+            </div>
+          ) : null}
+          {showRightNavbar ? (
+            <div className="right-navbar">
+              <h1 className="body-heading">Right Navbar</h1>
+              <div className="ad-box-container">
+                <p className="ad-box">Ad 1</p>
+                <p className="ad-box">Ad 2</p>
+              </div>
+            </div>
+          ) : null}
+        </div>
+      )
+    }}
+  </ConfigurationContext.Consumer>
+)
+
+export default Body
